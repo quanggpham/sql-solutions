b297d235ff390b0f08cc8c02b198eda98736c962
Quang Dzung Pham <132767821+quanggpham@users.noreply.github.com>
2026-08-25 12:38:35 +0700

    ok

--- 1.11.sql
+++ 1.11.sql
@@ -1,8 +1,19 @@
 -- 1.11	Trong mỗi phòng ban, hãy tìm 5 người đang làm việc giữ chức vụ staff có lương cao nhất.
-select *
+
+with ranked as (
+select de.emp_no, de.dept_no, s.salary,
+	row_number() over(partition by de.dept_no order by s.salary desc) as ranking
 from dept_emp de
 join titles t
-	on t.emp_no = de.emp_no
-    and t.title = 'Staff'
-    and t.to_date = '9999-01-01'
-where de.to_date = '9999-01-01'
+	on de.emp_no = t.emp_no
+join salaries s
+	on de.emp_no = s.emp_no
+where de.to_date  = '9999-01-01'
+	and t.title = 'Staff'
+    and s.to_date  = '9999-01-01'
+    and t.to_date  = '9999-01-01'
+)
+
+select *
+from ranked
+where ranking <= 5
